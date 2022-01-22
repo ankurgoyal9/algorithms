fc4bc4bfe74040830931cdedb1ee4af000967785
Ankur <ankurgoyal9@yahoo.co.in>
2022-01-23 00:47:59 +0530

    AarrayManipulation Initial

---
+++ src/main/java/com/algorithm/arrays/ArrayManipulation.java
@@ -0,0 +1,30 @@
+package com.algorithm.arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/crush/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+ * 
+ *  a b k
+    1 5 3
+    4 8 7
+    6 9 1
+Add the values of  between the indices  and  inclusive:
+index->	 1 2 3  4  5 6 7 8 9 10
+        [0,0,0, 0, 0,0,0,0,0, 0]
+        [3,3,3, 3, 3,0,0,0,0, 0]
+        [3,3,3,10,10,7,7,7,0, 0]
+        [3,3,3,10,10,8,8,8,1, 0]
+1	2	3	4	5	6	7	8	9	10
+3                  -3
+            7               -7
+                    1               -1
+3			7		-2			-7	-1
+
+Add values and get running total and save greatest value in a variable
+ */
+public class ArrayManipulation {
+
+    public static void main(String[] args){
+
+    }
+    
+}
---
+++ src/main/java/com/algorithm/arrays/NewYearChaos.java
@@ -0,0 +1,58 @@
+package com.algorithm.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+// https://www.hackerrank.com/challenges/new-year-chaos/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+public class NewYearChaos {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt).collect(Collectors.toList());
+
+                minimumBribes(q.stream().mapToInt(Integer::intValue).toArray());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
+    static void minimumBribes(int[] q) {
+        int bribes = 0;
+        int position = q.length;
+        for (int i = position - 1; i >= 0; --i) {
+            if (q[i] != i + 1) {
+                if (i - 1 >= 0 && q[i - 1] == i + 1) {
+                    q[i - 1] = q[i];
+                    q[i] = i + 1;
+                    bribes += 1;
+                } else if (i - 2 >= 0 && q[i - 2] == i + 1) {
+                    q[i - 2] = q[i - 1];
+                    q[i - 1] = q[i];
+                    q[i] = i + 1;
+                    bribes += 2;
+                } else {
+                    System.out.println("Too chaotic");
+                    return;
+                }
+            }
+        }
+        System.out.println(bribes);
+    }
+
+}
