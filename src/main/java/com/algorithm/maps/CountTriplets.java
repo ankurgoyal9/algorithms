27577112b8b629007a00dfb10ac900628e1282f4
Ankur <ankurgoyal9@yahoo.co.in>
2022-01-23 20:21:44 +0530

    Count Triplets

---
+++ src/main/java/com/algorithm/maps/CountTriplets.java
@@ -0,0 +1,90 @@
+package com.algorithm.maps;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/*
+    https://www.hackerrank.com/challenges/count-triplets-1/copy-from/201346253?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+*/
+public class CountTriplets {
+
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+
+        Map<Long, Long> rightMap = getOccurenceMap(arr);
+        Map<Long, Long> leftMap = new HashMap<>();
+        long numberOfGeometricPairs = 0;
+
+        for (long val : arr) {
+            long countLeft = 0;
+            long countRight = 0;
+            long lhs = 0;
+            long rhs = val * r;
+            if (val % r == 0) {
+                lhs = val / r;
+            }
+            Long occurence = rightMap.get(val);
+            rightMap.put(val, occurence - 1L);
+
+            if (rightMap.containsKey(rhs)) {
+                countRight = rightMap.get(rhs);
+            }
+            if (leftMap.containsKey(lhs)) {
+                countLeft = leftMap.get(lhs);
+            }
+            numberOfGeometricPairs += countLeft * countRight;
+            insertIntoMap(leftMap, val);
+        }
+        return numberOfGeometricPairs;
+    }
+
+    private static Map<Long, Long> getOccurenceMap(List<Long> test) {
+        Map<Long, Long> occurenceMap = new HashMap<>();
+        for (long val : test) {
+            insertIntoMap(occurenceMap, val);
+        }
+        return occurenceMap;
+    }
+
+    private static void insertIntoMap(Map<Long, Long> occurenceMap, Long val) {
+        if (!occurenceMap.containsKey(val)) {
+            occurenceMap.put(val, 1L);
+        } else {
+            Long occurence = occurenceMap.get(val);
+            occurenceMap.put(val, occurence + 1L);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new
+        // FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        System.out.println(ans);
+
+        // bufferedWriter.write(String.valueOf(ans));
+        // bufferedWriter.newLine();
+
+        // bufferedReader.close();
+        // bufferedWriter.close();
+    }
+}
